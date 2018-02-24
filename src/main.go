a279d5b500c24795f25f5cbc10eac0ee6f16ba1e
sterling <sterlinghoughton@gmail.com>
2018-02-24 10:45:21 -0800

    Create parseLines function to parse CSV file and breakout individual problems into structs, create problem struct, loop through problems/save answer/compare answer and save to correct counter if correct

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,8 +21,42 @@ func main() {
 	if err != nil {
 		exit("Failed to parse provided CSV file.")
 	}
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		// Scanf will get rid of all spaces/trailing spaces
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
+// Since length is already known, no need to let the append function
+// do work in resizing the slice when it needs to.
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: strings.TrimSpace(line[0]),
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+// breakout problems into a struct for more flexability
+type problem struct {
+	q string
+	a string
+}
+
+// easy helper function for errors
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
